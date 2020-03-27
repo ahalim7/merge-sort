63cb2998ef3e4fae7b848b4fbd8abc819c6cba1d
ahalim7 <ahalim7@yahoo.com>
2020-03-27 12:00:52 +0200

    First Commit

---
+++ src/main/java/halim/lessons/mergesort/CountingInversions.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package halim.lessons.mergesort;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author AbdElHalim
+ */
+public class CountingInversions {
+
+    public static void main(String[] args) {
+        long result = mergeSortAndCount(new int[]{7, 1, 2, 1, 2}, 0, 4);
+        System.out.println(result);
+    }
+
+    // Function to count the number of inversions 
+    // during the merge process 
+    private static int mergeAndCount(int[] arr, int l, int m, int r) {
+
+        // Left subarray 
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+        // Right subarray 
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l, swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+
+        // Fill from the rest of the left subarray 
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+
+        // Fill from the rest of the right subarray 
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+
+        return swaps;
+    }
+
+    // Merge sort function 
+    private static int mergeSortAndCount(int[] arr, int l, int r) {
+
+        // Keeps track of the inversion count at a 
+        // particular node of the recursion tree 
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            // Total inversion count = left subarray count 
+            // + right subarray count + merge count 
+            // Left subarray count 
+            count += mergeSortAndCount(arr, l, m);
+
+            // Right subarray count 
+            count += mergeSortAndCount(arr, m + 1, r);
+
+            // Merge count 
+            count += mergeAndCount(arr, l, m, r);
+        }
+
+        return count;
+    }
+}
